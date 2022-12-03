db45624187634a2dd97913109faa4bee0da79457
agurokeendavid <agurokeendavid@gmail.com>
2022-12-04 02:00:01 +0800

    Finished variables topic.

--- VariablesLesson/Program.cs
+++ VariablesLesson/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace VariablesLesson
+﻿using System;
+
+namespace VariablesLesson
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            int x; // declaration
+            x = 123; // initialization
+
+            int y = 321; // declaration + initialization
+
+            int z = x + y;
+
+            int age = 21; // store whole integer
+            double height = 300.5; // decimal number
+            bool alive = false; // true or false
+            char symbol = '@';
+            String name = "Bro";
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine($"Your age is {age}");
+            Console.WriteLine($"Your height is {height} cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            String userName = symbol + name;
+
+            Console.WriteLine("Your username is " + userName);
         }
     }
 }
