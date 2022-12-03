6b96537c92c659d7695114500c0895222e8850b8
agurokeendavid <agurokeendavid@gmail.com>
2022-12-04 02:55:20 +0800

    string methods topic.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fullName = "Bro Code";
+            string phoneNumber = "123-456-7890";
+
+            // fullName = fullName.ToUpper();
+            // fullName = fullName.ToLower();
+            // phoneNumber = phoneNumber.Replace("-", "");
+            // Console.WriteLine(phoneNumber);
+            // Console.WriteLine(fullName);
+
+            // String userName = fullName.Insert(0, "Mr. ");
+            // Console.WriteLine(userName);
+
+            // Console.WriteLine(fullName.Length);
+
+            String firstName = fullName.Substring(0, 3);
+            String lastName = fullName.Substring(4, 4);
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+    }
+}
