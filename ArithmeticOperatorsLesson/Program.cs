19057d0520eccc57690f86b4022899a592091e4d
agurokeendavid <agurokeendavid@gmail.com>
2022-12-04 02:32:27 +0800

    ArithmeticLesson topic.

---
+++ ArithmeticOperatorsLesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArithmeticOperatorsLesson
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double friends = 10;
+            // friends = friends + 1;
+            // friends += + 2;
+            // friends++;
+
+            // friends = friends - 1;
+            // friends -= 1;
+            // friends--;
+
+            // friends = friends * 2;
+            // friends *= 2;
+
+            // friends = friends / 2;
+            // friends /= 2;
+
+            double remainder = friends % 2;
+
+            Console.WriteLine(remainder);
+        }
+    }
+}
