b5ace3a071af7e9404f6e417ac7c7991f39e4ae4
agurokeendavid <agurokeendavid@gmail.com>
2022-12-04 02:12:02 +0800

    type casting in c#

---
+++ ConstantsLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConstantsLesson
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // constants = immutable values which are known at compile time and do not change for the life of the program.
+
+            double pi = 3.14159;
+
+            Console.WriteLine(pi);
+        }
+    }
+}
