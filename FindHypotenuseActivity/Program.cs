faa7c71c3c0d248146f4635f0160f293eb6780e2
agurokeendavid <agurokeendavid@gmail.com>
2022-12-04 02:47:18 +0800

    hypotenuse activity.

---
+++ FindHypotenuseActivity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FindHypotenuseActivity
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter side B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine($"The hypotenuse of {a} and {b} is {c}");
+        }
+    }
+}
